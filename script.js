f716c673cd328766e1c8c50073013f3e6542da90
Max <max_lisyanskiy@mail.ru>
2020-12-21 00:24:26 +0300

    practise 3

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели', 'Укажите количество');
+let numberOfFilms;
 
-    let personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
 
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели', 'Укажите количество');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели', 'Укажите количество');
+    }
+}
+
+// start();
+
+//Global Object
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function detectiveFilms(){
     for (let i = 0; i < 2; i++){
         var question = prompt('Один из последних просмотренных фильмов?', ''),
             answer = prompt('На сколько оцените его?', '');
@@ -21,7 +34,10 @@ let numberOfFilms = +prompt('Сколько фильмов вы посмотре
         }
         
     }
+}
+// detectiveFilms();
 
+function personalLevel(){
     if(personalMovieDB.count < 10) {
         document.write("Мало");
     } else if (personalMovieDB.count == 10 || personalMovieDB.count < 30) {
@@ -31,8 +47,21 @@ let numberOfFilms = +prompt('Сколько фильмов вы посмотре
     } else {
         document.write("Ошибка");
     }
+}
+// personalLevel();
+let writeYourGenres = () =>{
+    for(let i = 1; i < 4; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+writeYourGenres();
 
+let showMyBD = function(){
+    if (personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
+}
+showMyBD();
 
-    console.log(personalMovieDB);
 
 
