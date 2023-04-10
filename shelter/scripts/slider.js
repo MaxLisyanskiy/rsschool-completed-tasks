244276f7c132a2d93c9e2e7e657f169bb4de09ed
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-04-10 12:20:45 +0300

    feat: add slider script file

---
+++ shelter/scripts/slider.js
@@ -0,0 +1,13 @@
+let pastArr = [];
+let currArr = [];
+let nextArr = [];
+
+function init() {}
+
+function forward() {}
+
+function changeToBackward() {}
+
+function backward() {}
+
+function changeToForward() {}
