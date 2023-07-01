2214eada5a2ba31b3d9b1836b390c53c7c819ff6
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-07-01 15:07:51 +0300

    feat: add some elements in main and styles

--- rs-css/src/app/components/AppMain/AppMain.ts
+++ rs-css/src/app/components/AppMain/AppMain.ts
@@ -9,7 +9,7 @@ export default class AppMain {
   formBtnEnter: HTMLButtonElement = createButtonElement("form__enter", "Enter", "submit");
   formBtnHelp: HTMLButtonElement = createButtonElement("form__help", "Help?", "button");
 
-  htmlViewer: HTMLElement = document.createElement("div");
+  htmlViewer: HTMLElement = createElement("div", "layout__html");
 
   private createFormRowNumbers(className: string): HTMLElement {
     const numbers = createElement("div", className);
@@ -28,7 +28,18 @@ export default class AppMain {
 
     // this.formBtnEnter = createButtonElement("form__enter", "Enter", "submit");
 
-    this.form.append(this.formInput, this.formBtnEnter, this.formBtnHelp);
+    const formInputWrapp = createElement("div", "form__wrapp");
+    formInputWrapp.append(this.formInput, this.formBtnEnter, this.formBtnHelp);
+
+    const formDescription = createElement("div", "form__descr");
+    formDescription.innerHTML = `
+      {
+        <br>
+        <span>/* Styles would go here. */</span>
+        <br>
+      }
+    `;
+    this.form.append(formInputWrapp, formDescription);
 
     return this.form;
   }
@@ -45,14 +56,14 @@ export default class AppMain {
 
     const pane = document.createElement("div");
     pane.classList.add("editor__item", "pane");
-    const paneHeader = createTextElement("p", "pane__title", "CSS Editor style.css");
+    const paneHeader = createTextElement("p", "pane__title", "CSS Editor <span>style.css</span>");
     const formWrapper = createElement("div", "pane__wrapp");
     formWrapper.append(this.createFormRowNumbers("pane__numbers"), this.createForm());
     pane.append(paneHeader, formWrapper);
 
     const layout = document.createElement("div");
     layout.classList.add("editor__item", "layout");
-    const layoutHeader = createTextElement("p", "layout__title", "HTML Viewer table.html");
+    const layoutHeader = createTextElement("p", "layout__title", "HTML Viewer <span>table.html</span>");
     const layoutWrapper = createElement("div", "layout__wrapp");
     layoutWrapper.append(this.createFormRowNumbers("layout__numbers"), this.htmlViewer);
     layout.append(layoutHeader, layoutWrapper);
